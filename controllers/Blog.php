50a92005a97c0844745a29dc6e8066746440f76f
kleber <ruzkleber@gmail.com>
2018-07-24 14:41:57 -0300

    fix role access and visibility of posts

--- controllers/Blog.php
+++ controllers/Blog.php
@@ -58,12 +58,14 @@ class Blog extends Front_Controller{
         }else{  $this->session->unset_userdata('search'); }
       }
 
-        $where = array('blog_posts.deleted'=>0);
+       $find_in_set = "FIND_IN_SET(".$this->current_user->role_id.", ".$this->db->dbprefix."blog_posts.roles_access)";
+
+       $where = array('blog_posts.deleted'=>0);
 
         $this->blog_model->select("title_post,slug_post,preview_image,blog_posts.created_on as created_on,email,display_name,photo_avatar,username");
         $this->blog_model->order_by('blog_posts.created_on','desc');
         $this->blog_model->join('users','blog_posts.created_by  = users.id','left');
-        $this->blog_model->limit(6, $offset)->where($where);
+        $this->blog_model->limit(6, $offset)->where($where)->where($find_in_set)->or_where('created_by',$this->current_user->id)->where($where);
         if(isset($search_text)){ $this->blog_model->like('title_post',$search_text); }
         $posts = $this->blog_model->find_all();
 
@@ -72,7 +74,7 @@ class Blog extends Front_Controller{
         $this->pager['base_url']    = base_url()."blog/index/";
         $this->pager['per_page']    = 6;
         if(isset($search_text)){ $this->blog_model->like('title_post',$search_text); }
-        $this->pager['total_rows']  = $this->blog_model->where($where)->count_all();
+        $this->pager['total_rows']  = $this->blog_model->where($where)->where($find_in_set)->or_where('created_by',$this->current_user->id)->where($where)->count_all();
         $this->pager['uri_segment'] = 3;
 
         $this->pagination->initialize($this->pager);
@@ -158,8 +160,17 @@ class Blog extends Front_Controller{
             redirect('blog');
         }
 
-         $this->db->join('users','blog_posts.created_by  = users.id','left');
+        $this->db->join('users','blog_posts.created_by  = users.id','left');
+        $this->db->where('blog_posts.deleted',0);
         if($post = $this->blog_model->find_by('slug_post',$id)){
+
+          if($post->created_by != $this->current_user->id and !in_array($this->current_user->role_id,explode(',',$post->roles_access))){
+
+            Template::set_message('Sem permissão para acessar o conteúdo.', 'danger');
+            Template::redirect('blog');
+
+             }
+
 
           $this->load->library('blog/Nested_set');
           $this->nested_set->setControlParams('blog_categories','lft','rgt','id_category','parent_category','name_category');
