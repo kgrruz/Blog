a26f48eefee9750f22384dd9731571608ea4c6a7
kleber <ruzkleber@gmail.com>
2018-12-01 13:52:59 -0200

    Change module version.

--- config/config.php
+++ config/config.php
@@ -8,6 +8,6 @@ $config['module_config'] = array(
 	'label'       => array('english'=>'Blog','portuguese_br'=>'Blog'),
 	'route'       => 'blog',
 	'visible_module' => true,
-	'version'		=> '1.0.4',
+	'version'		=> '1.0.5',
 	'author'		=> 'admin',
 );
