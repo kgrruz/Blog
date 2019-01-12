44e4b513b4ca398c2288a1fde4fec02763096b92
kleber <ruzkleber@gmail.com>
2019-01-11 22:51:05 -0200

    Fix delivery notification on blog create.

--- controllers/Blog.php
+++ controllers/Blog.php
@@ -256,7 +256,7 @@ class Blog extends Front_Controller{
                 $blog = $this->blog_model->find($insert_id);
 
                 $id_act = log_activity($this->auth->user_id(), '[blog_act_create_record] : ' . '<a href="blog/post/'.$blog->slug_post.'">'.$blog->title_post.'</a>', 'blog');
-                $ids = $this->user_model->get_id_users_role('id',$this->input->post('roles_access'));
+                $ids = $this->user_model->get_id_users_role('id',explode(",",$blog->roles_access));
                 log_notify($ids, $id_act);
 
                 $this->send_blog_email($ids,$blog);
