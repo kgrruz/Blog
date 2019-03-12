0a344c540eb690dacf66aacb2e14554a96209111
kleber <ruzkleber@gmail.com>
2019-03-12 11:04:03 -0300

    Minor layout changes.

--- views/widgets/news.php
+++ views/widgets/news.php
@@ -1,15 +1,23 @@
-<h4 class="mb-3"><?php echo lang("blog_news"); ?></h4>
 
-<?php if($posts){ foreach($posts as $post){ ?>
+<?php if($posts){ ?>
 
-     <div class="media mb-3">
-       <img class="mr-3" src="<?php echo base_url(); ?>images/<?php echo $post->preview_image; ?>?module=blog&width=100&assets=assets/images/posts_preview" alt="image_preview">
+  <div class="card  mb-3">
+  <div class="card-header">
+    <?php echo lang("blog_news"); ?>
+  </div>
+  <ul class="list-group list-group-flush">
+
+<?php foreach($posts as $post){ ?>
+  <li class="list-group-item">
+     <div class="media">
+       <img class="mr-3" style="height:67px;width:100px" src="<?php echo base_url(); ?>images/<?php echo ($img = $post->preview_image)?$img:'no_image.png'; ?>?module=blog&width=100&assets=assets/images/posts_preview" alt="image_preview">
       <div class="media-body">
         <h5 class="mt-0"><?php echo anchor('blog/post/'.$post->slug_post, $post->title_post); ?></h5>
         <?php echo ut_date($post->created_on,'F j, Y H:i'); ?>
        </div>
     </div>
-
+</li>
    <?php } ?>
-
+</ul>
+</div>
  <?php } ?>
