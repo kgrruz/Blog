1ee7f5a09388c08ab4f8ea842426c88e94b1232e
kleber <ruzkleber@gmail.com>
2019-04-08 14:33:56 -0300

    Remove undefinied variable.

--- controllers/Blog_events.php
+++ controllers/Blog_events.php
@@ -21,7 +21,7 @@ class Blog_events{
     $this->CI->blog_model->select("title_post,slug_post,preview_image,blog_posts.created_on as created_on,email,display_name,photo_avatar,username");
     $this->CI->blog_model->order_by('blog_posts.created_on','desc');
     $this->CI->blog_model->join('users','blog_posts.created_by  = users.id','left');
-    $this->CI->blog_model->limit(6, $offset)->where($where);
+    $this->CI->blog_model->limit(6)->where($where);
     if(isset($search_text)){ $this->CI->blog_model->like('title_post',$search_text); }
     $data['posts'] = $this->CI->blog_model->find_all();
 
