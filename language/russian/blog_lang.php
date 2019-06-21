f61d1b043b5b5cdf1da890ac5f6e4523945ad41e
kleber <ruzkleber@gmail.com>
2019-06-21 13:57:06 -0300

    Add russian language.

---
+++ language/russian/blog_lang.php
@@ -0,0 +1,115 @@
+<?php defined ('BASEPATH') || exit ('No direct script access allowed');
+
+$lang['blog_manage'] = 'Управление блогом';
+$lang['blog_edit'] = 'Edit';
+$lang['blog_true'] = 'True';
+$lang['blog_false'] = 'False';
+$lang['blog_create'] = 'Создать';
+$lang['blog_list'] = 'Список';
+$lang['blog_new'] = 'New';
+$lang['blog_post_created_by'] = 'by';
+$lang['blog_know_more'] = 'Узнать больше';
+$lang['blog_edit_text'] = 'Редактировать это по мере необходимости';
+$lang['blog_no_records'] = 'В системе нет сообщений.';
+$lang['blog_create_new'] = 'Создать новую запись.';
+$lang['blog_create_success'] = 'Пост успешно создан.';
+$lang['blog_create_failure'] = 'При создании сообщения возникла проблема:';
+$lang['blog_create_new_button'] = 'Создать новую запись';
+$lang['blog_invalid_id'] = 'Неверный идентификатор сообщения.';
+$lang['blog_edit_success'] = 'Запись успешно сохранена.';
+$lang['blog_edit_failure'] = 'При сохранении сообщения возникла проблема:';
+$lang['blog_delete_success'] = 'Успешно удаленные записи.';
+$lang['blog_delete_failure'] = 'Не удалось удалить запись:';
+$lang['blog_delete_error'] = 'Вы не выбрали ни одной записи для исключения.';
+$lang['blog_actions'] = 'Actions';
+$lang['blog_cancel'] = 'Отмена';
+$lang['blog_delete_record'] = 'Удалить это сообщение';
+$lang['blog_delete_confirm'] = 'Вы уверены, что хотите удалить это сообщение?';
+$lang['blog_comment_approve_confirm'] = 'Вы уверены, что хотите одобрить этот комментарий?';
+$lang['blog_comment_approve_success'] = 'Успешно одобренный комментарий.';
+$lang['blog_edit_heading'] = 'Редактировать сообщение';
+$lang['blog_empty_posts'] = 'Блог без постов.';
+$lang['blog_roles'] = 'Доступно для';
+$lang['blog_enable_comments'] = 'Разрешить комментарии';
+$lang['blog_enable_attach'] = 'Разрешить загрузку в комментариях';
+$lang['blog_send_email_success'] = 'Письмо успешно отправлено.';
+$lang['blog_lets_comment'] = 'Оставьте свой комментарий:';
+$lang['blog_comments'] = 'Комментарии';
+$lang['blog_permission_folder'] = 'Дать разрешение на запись в следующую папку:';
+$lang['blog_minute'] = "минуты (с)";
+$lang['blog_hour'] = "время (с)";
+$lang['blog_day'] = "day (s)";
+$lang['blog_month'] = "month (s)";
+$lang['blog_news'] = "Новости";
+$lang['blog_settings_create_success'] = "Конфигурация успешно сохранена.";
+$lang['blog_flood_comment'] = "Подождите до следующего комментария:";
+$lang['blog_subject_new_comment'] = "Новый комментарий к:";
+$lang['blog_subject_new_comment_mod'] = "Комментарий ожидает вашего одобрения:";
+$lang['blog_new_comment_email_success'] = "Письмо успешно отправлено.";
+
+// Создание / редактирование кнопок
+$lang['blog_action_edit'] = 'Редактировать сообщение';
+$lang['blog_action_create'] = 'Опубликовать сообщение';
+
+// Мероприятия
+$lang['blog_act_create_record'] = 'Отправленная запись';
+$lang['blog_act_edit_record'] = 'Обновленная запись с идентификатором';
+$lang['blog_act_delete_record'] = 'Удаленная запись с идентификатором';
+
+// Особенности листинга
+$lang['blog_records_empty'] = 'Записи не найдены.';
+$lang['blog_errors_message'] = 'Пожалуйста, исправьте следующие ошибки:';
+
+// Заголовки столбцов
+$lang['blog_column_created'] = 'Создано';
+$lang['blog_column_deleted'] = 'Удалено';
+$lang['blog_column_modified'] = 'Обновлено';
+$lang['blog_column_name'] = 'Post';
+$lang['blog_column_description'] = 'Описание';
+$lang['blog_column_categ'] = 'Category';
+$lang['blog_column_ec'] = 'Комментарии';
+$lang['blog_column_comment'] = 'Комментарий';
+$lang['blog_column_ea'] = 'Вложения';
+$lang['blog_column_created_by'] = 'Автор';
+
+// Детали модуля
+$lang['blog_module_name'] = 'Blog';
+$lang['blog_module_description'] = 'Управлять своим постом';
+$lang['blog_area_title'] = 'Посты';
+
+// Поля
+$lang['blog_field_name'] = 'Заголовок сообщения';
+$lang['blog_field_upload_picture'] = 'Предварительный просмотр загрузки изображения';
+$lang['blog_field_picture'] = 'Preview';
+$lang['blog_field_description'] = 'Опубликовать контент';
+$lang['blog_field_category'] = 'Категория';
+$lang['blog_roles_access'] = 'Опубликовать группы доступа';
+$lang['blog_field_body'] = 'Содержание сообщения';
+
+$lang['blog_search'] = 'Поиск';
+$lang['blog_search_placeholder'] = 'Поиск сообщений ...';
+$lang['blog_image_preview_edit_overwrite'] = 'Заменит существующее изображение. Если там есть.';
+
+
+// настройки электронной почты
+$lang['blog_new_post'] = 'Новая запись в блоге';
+$lang['blog_new_post_desc'] = 'Получить уведомление для каждого нового сообщения в блоге';
+
+$lang['blog_settings'] = 'Настройки';
+$lang['blog_settings_post_visibility'] = 'Видимость сообщений';
+$lang['blog_settings_only_logged'] = 'Только для входа';
+$lang['blog_settings_post_public'] = 'Public';
+
+$lang['blog_settings_email_enever'] = 'Отправить мне письмо';
+$lang['blog_settings_new_comments'] = 'Весь новый комментарий';
+$lang['blog_settings_new_comment_mod'] = 'Новый комментарий ожидает модерации';
+
+$lang['blog_settings_before_comments_appear'] = 'До публикации комментария';
+$lang['blog_settings_comment_must_be_approved'] = 'Комментарии должны быть одобрены';
+$lang['blog_settings_close_comments'] = 'Заблокировать комментарии после';
+$lang['blog_settings_comment_flood_time'] = 'Время между комментариями';
+
+$lang['blog_comment_delete_confirm'] = 'Вы уверены, что хотите удалить комментарий?';
+$lang['blog_invalid_comment_id'] = 'Идентификатор комментария недействителен.';
+$lang['blog_comment_delete_success'] = 'Комментарий удален.';
+$lang['blog_comment_file'] = 'Файл';
---
+++ language/russian/category_lang.php
@@ -0,0 +1,68 @@
+<?php defined('BASEPATH') || exit('No direct script access allowed');
+
+$lang['category_manage'] = 'Управление категорией';
+$lang['category_edit'] = 'Редактировать';
+$lang['category_true'] = 'True';
+$lang['category_false'] = 'False';
+$lang['category_create'] = 'Добавить';
+$lang['category_list'] = 'Список';
+$lang['category_new'] = 'New';
+$lang['category_edit_text'] = 'Отредактируйте это в соответствии со своими потребностями';
+$lang['category_no_records'] = 'В системе нет категории.';
+$lang['category_create_new'] = 'Создать новую категорию.';
+$lang['category_create_success'] = 'Категория успешно создана.';
+$lang['category_create_failure'] = 'При создании категории возникла проблема:';
+$lang['category_create_new_button'] = 'Создать новую категорию';
+$lang['category_invalid_id'] = 'Неверный идентификатор категории';
+$lang['category_edit_success'] = 'категория успешно сохранена.';
+$lang['category_edit_failure'] = 'Не удалось сохранить категорию:';
+$lang['category_delete_success'] = 'запись (и) успешно удалена.';
+$lang['category_delete_failure'] = 'Не удалось удалить запись:';
+$lang['category_delete_error'] = 'Вы не выбрали ни одной записи для удаления.';
+$lang['category_actions'] = 'Actions';
+$lang['category_cancel'] = 'Отмена';
+$lang['category_delete_record'] = 'Удалить эту категорию';
+$lang['category_delete_confirm'] = 'Хотите такие же, как эта категория (и)?';
+$lang['category_edit_heading'] = 'с категорией';
+$lang['category_empty'] = 'Нет зарегистрированных категорий.';
+$lang['category_first_categ_name'] = 'General';
+$lang['category_first_categ_slug'] = 'general';
+$lang['category_first_categ_desc'] = 'Общие проблемы';
+
+// Создание / редактирование кнопок
+$lang['category_action_edit'] = 'Сохранить категорию';
+$lang['category_action_create'] = 'Зарегистрировать категорию';
+
+// Мероприятия
+$lang['category_act_create_record'] = 'Создана запись с идентификатором';
+$lang['category_act_edit_record'] = 'Запись обновлена ​​с идентификатором';
+$lang['category_act_delete_record'] = 'Запись удалена с идентификатором';
+
+// Особенности листинга
+$lang['category_records_empty'] = 'Не найдено записей, соответствующих вашему выбору.';
+$lang['category_errors_message'] = 'Пожалуйста, исправьте следующие ошибки:';
+
+// Колонка заголовков
+$lang['category_column_name'] = 'Name';
+$lang['category_column_items'] = 'Items';
+$lang['category_column_created'] = 'Создано';
+$lang['category_column_deleted'] = 'Удалено';
+$lang['category_column_modified'] = 'Modified';
+$lang['category_column_deleted_by'] = 'Исключено';
+$lang['category_column_created_by'] = 'Создано';
+$lang['category_column_modified_by'] = 'Изменено';
+
+// Детали модуля
+$lang['category_module_name'] = 'Category';
+$lang['category_module_description'] = 'Управление вашей категорией';
+$lang['category_area_title'] = 'Категории';
+$lang['category_desc_form_create'] = 'Пишите в свои посты и организуйте их.';
+
+// Поля
+$lang['category_field_name_category'] = 'Имя';
+$lang['category_field_category_description'] = 'Описание';
+$lang['category_field_item_price'] = 'цена';
+$lang['category_field_image_category'] = 'Значок категории';
+$lang['category_field_parent_category'] = 'Родительская категория';
+
+$lang['category_select_placeholder'] = 'Select ...';
