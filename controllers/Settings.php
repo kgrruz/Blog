d155a102e4039dd0a18acf14150a002e3e5539c8
kgr <admin@wgstor.com>
2020-06-03 17:49:51 -0300

    Add media uploads restrictions settings.

--- controllers/Settings.php
+++ controllers/Settings.php
@@ -23,8 +23,6 @@ class Settings extends Admin_Controller
         $this->load->model('blog/blog_model');
         $this->lang->load('blog');
 
-        $this->form_validation->set_error_delimiters("<span class='alert alert-danger'>", "</span>");
-
         Template::set_block('sub_nav', 'settings/_sub_nav');
 
         Assets::add_module_js('blog', 'blog.js');
@@ -47,6 +45,10 @@ class Settings extends Admin_Controller
            $this->form_validation->set_rules('block_post_after_period', 'lang:blog_settings_close_comments', 'required');
            $this->form_validation->set_rules('comment_flood', 'lang:blog_settings_comment_flood_time', 'required|integer');
            $this->form_validation->set_rules('comment_flood_period', 'lang:blog_settings_comment_flood_time', 'required');
+           $this->form_validation->set_rules('maxsize_preview_image', 'lang:blog_settings_maxsize_preview_image', 'required|integer');
+           $this->form_validation->set_rules('maxsize_body_image', 'lang:blog_settings_maxsize_body_image', 'required|integer');
+           $this->form_validation->set_rules('maxsize_comment_attach', 'lang:blog_settings_maxsize_comment_attach', 'required|integer');
+           $this->form_validation->set_rules('allow_types_attach', 'lang:blog_settings_allow_types_attach', 'required');
 
            if ($this->form_validation->run() == FALSE){
 
@@ -61,7 +63,11 @@ class Settings extends Admin_Controller
             array('name'=>'blog.email_new_comment_mod', 'value'=>$this->input->post("email_new_comment_mod")),
             array('name'=>'blog.must_aprove_comment', 'value'=>$this->input->post("must_aprove_comment")),
             array('name'=>'blog.block_post_after', 'value'=>$this->input->post("block_post_after").' '.$this->input->post("block_post_after_period")),
-            array('name'=>'blog.comment_flood', 'value'=>$this->input->post("comment_flood").' '.$this->input->post("comment_flood_period"))
+            array('name'=>'blog.comment_flood', 'value'=>$this->input->post("comment_flood").' '.$this->input->post("comment_flood_period")),
+            array('name'=>'blog.maxsize_preview_image', 'value'=>$this->input->post("maxsize_preview_image")),
+            array('name'=>'blog.maxsize_body_image', 'value'=>$this->input->post("maxsize_body_image")),
+            array('name'=>'blog.maxsize_comment_attach', 'value'=>$this->input->post("maxsize_comment_attach")),
+            array('name'=>'blog.allow_types_attach', 'value'=>$this->input->post("allow_types_attach"))
           );
 
           $updated = $this->settings_lib->update_batch($data);
--- views/settings/index.php
+++ views/settings/index.php
@@ -34,7 +34,7 @@
          </div>
                  <div class="form-check">
 
-               <input type="checkbox" class="form-check-input" name="email_new_comment_mod"<?php echo ($settings['blog.email_new_comment_mod'] == 1)? "checked":""; ?> id="email_new_comment_mod" value="1"  />
+               <input type="checkbox" class="form-check-input" name="email_new_comment_mod" <?php echo ($settings['blog.email_new_comment_mod'] == 1)? "checked":""; ?> id="email_new_comment_mod" value="1"  />
                  <label class="form-check-label" for="new_comment_mod">  <?php echo lang("blog_settings_new_comment_mod"); ?>
            </label>
          </div>
@@ -58,10 +58,10 @@
          <div class="col-sm-8">
          <div class="row">
          <div class="col-sm-6">
-               <input type="number" class="form-control" min="1" value="<?php echo set_value('block_post_after', isset($bloc_post[0]) ? $bloc_post[0] : 1); ?>" name="block_post_after" id="block_post_after" />
+               <input type="number" class="form-control form-control-sm" min="1" value="<?php echo set_value('block_post_after', isset($bloc_post[0]) ? $bloc_post[0] : 1); ?>" name="block_post_after" id="block_post_after" />
            </div>
             <div class="col-sm-6">
-              <select class="form-control" name="block_post_after_period" id="block_post_after_period">
+              <select class="form-control form-control-sm" name="block_post_after_period" id="block_post_after_period">
                 <option value="hour" <?php echo (isset($bloc_post[1]) and $bloc_post[1] == "hour")? "selected":""; ?> ><?php echo lang("blog_hour");?></option>
                 <option value="day" <?php echo (isset($bloc_post[1]) and $bloc_post[1] == "day")? "selected":""; ?>><?php echo lang("blog_day");?></option>
                 <option value="month" <?php echo (isset($bloc_post[1]) and $bloc_post[1] == "month")? "selected":""; ?> ><?php echo lang("blog_month");?></option>
@@ -76,10 +76,10 @@
          <div class="col-sm-8">
          <div class="row">
            <div class="col-sm-6">
-                 <input type="number" class="form-control" min="1" name="comment_flood" value="<?php echo set_value('comment_flood', isset($comment_flood[0]) ? $comment_flood[0] : 1); ?>"  id="comment_flood" />
+                 <input type="number" class="form-control form-control-sm" min="1" name="comment_flood" value="<?php echo set_value('comment_flood', isset($comment_flood[0]) ? $comment_flood[0] : 1); ?>"  id="comment_flood" />
              </div>
               <div class="col-sm-6">
-                <select class="form-control" name="comment_flood_period" id="bcomment_flood_period">
+                <select class="form-control form-control-sm" name="comment_flood_period" id="bcomment_flood_period">
                   <option value="minute" <?php echo (isset($comment_flood[1]) and $comment_flood[1] == "minute")? "selected":""; ?>><?php echo lang("blog_minute");?></option>
                   <option value="hour" <?php echo (isset($comment_flood[1]) and $comment_flood[1] == "hour")? "selected":""; ?>><?php echo lang("blog_hour");?></option>
                   <option value="day" <?php echo (isset($comment_flood[1]) and $comment_flood[1] == "day")? "selected":""; ?>><?php echo lang("blog_day");?></option>
@@ -89,6 +89,41 @@
      </div>
      </div>
      </div>
+
+
+     <div class="form-group row<?php echo form_error('maxsize_preview_image') ? $errorClass : ''; ?>">
+         <label class="col-sm-4 col-form-label" for="maxsize_preview_image"><?php echo lang("blog_settings_maxsize_preview_image"); ?></label>
+         <div class="col-sm-8">
+                 <input type="number" class="form-control form-control-sm w-auto d-inline" min="1" required name="maxsize_preview_image" value="<?php echo set_value('maxsize_preview_image', $settings['blog.maxsize_preview_image']); ?>"  id="maxsize_preview_image" />
+                 <span class="pt-1">MB</span>
+     </div>
+     </div>
+
+     <div class="form-group row<?php echo form_error('maxsize_body_image') ? $errorClass : ''; ?>">
+         <label class="col-sm-4 col-form-label" for="maxsize_body_image"><?php echo lang("blog_settings_maxsize_body_image"); ?></label>
+         <div class="col-sm-8">
+                 <input type="number" class="form-control form-control-sm w-auto d-inline" min="1" required name="maxsize_body_image" value="<?php echo set_value('maxsize_body_image', $settings['blog.maxsize_body_image']); ?>"  id="maxsize_body_image" />
+                 <span class="pt-1">MB</span>
+     </div>
+     </div>
+
+     <div class="form-group row<?php echo form_error('maxsize_comment_attach') ? $errorClass : ''; ?>">
+         <label class="col-sm-4 col-form-label" for="maxsize_comment_attach"><?php echo lang("blog_settings_maxsize_comment_attach"); ?></label>
+         <div class="col-sm-8">
+                 <input type="number" class="form-control form-control-sm w-auto d-inline" min="1" required name="maxsize_comment_attach" value="<?php echo set_value('maxsize_comment_attach', $settings['blog.maxsize_comment_attach']); ?>"  id="maxsize_comment_attach" />
+                 <span class="pt-1">MB</span>
+     </div>
+     </div>
+
+     <div class="form-group row<?php echo form_error('allow_types_attach') ? $errorClass : ''; ?>">
+         <label class="col-sm-4 col-form-label" for="allow_types_attach"><?php echo lang("blog_settings_allow_types_attach"); ?></label>
+         <div class="col-sm-8">
+                 <input type="text" class="form-control form-control-sm w-auto d-inline" min="1" required name="allow_types_attach" value="<?php echo set_value('allow_types_attach', $settings['blog.allow_types_attach']); ?>"  id="allow_types_attach" />
+          
+     </div>
+     </div>
+
+
      </div>
      </div>
 <div class="card-footer">
@@ -96,7 +131,7 @@
      <div class='row'>
      <div class='col-sm-4'></div>
      <div class="col-sm-8">
-         <input type='submit' name='save' class='btn btn-success mx-0' value="<?php echo lang('bf_action_save'); ?>" />
+         <input type='submit' name='save' class='btn btn-sm btn-success mx-0' value="<?php echo lang('bf_action_save'); ?>" />
      </div>
      </div>
      </div>
